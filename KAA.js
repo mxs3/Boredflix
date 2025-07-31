1f657dff20f00fbec86da321cdf8f79905485710
MxS3 <elsanea2019@gmail.com>
2025-07-31 04:05:58 +0300

    Update KAA.js

--- KAA.js
+++ KAA.js
@@ -1,25 +1,31 @@
 async function searchResults(keyword) {
-  const apiKey = "f545e0c7b78f5294a6316e4a1a467b45"; // مفتاح TMDB الظاهر
-  const url = `https://api.themoviedb.org/3/search/multi?query=${encodeURIComponent(keyword)}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`;
+    const results = [];
 
-  try {
-    const res = await fetch(url);
-    const data = await res.json();
+    const res = await fetchv2("https://kaa.to/api/search", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query: keyword })
+    });
 
-    const results = (data.results || [])
-      .filter(item => item.media_type === "movie" || item.media_type === "tv")
-      .map(item => ({
-        title: item.title || item.name || "Untitled",
-        image: item.poster_path
-          ? `https://image.tmdb.org/t/p/w500${item.poster_path}`
-          : "",
-        href: `https://boredflix.com/watch/${item.media_type}/${item.id}`
-      }));
+    const json = await res.json();
+
+    for (const item of json) {
+        const title = item.title_en || item.title || "No Title";
+        const slug = item.slug;
+        const poster = item.poster?.hq
+            ? `https://img.kaa.to/posters/${item.poster.hq}.webp`
+            : null;
+
+        results.push({
+            title: title,
+            image: poster,
+            href: `https://kaa.to/${slug}`
+        });
+    }
 
     return JSON.stringify(results);
-  } catch (err) {
-    return JSON.stringify([{ title: "Error loading results", image: "", href: "" }]);
-  }
 }
 
 // ✅ فك ترميز HTML
